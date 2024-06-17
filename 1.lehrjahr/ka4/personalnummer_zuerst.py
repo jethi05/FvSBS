79e1bfaebe46a41f4ce2f83cb9108c799aa1fd72
jethi05 <jethi.gei@gmail.com>
2024-06-17 08:55:33 +0200

    Personalnummer.py hinzugefügt

---
+++ 1.lehrjahr/ka4/personalnummer_zuerst.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+neue_liste=[]
+wirth = [
+    "Mueller|Josef|FR_1112|Freiburg",
+    "Maier|Fritz|ST_1114|Vaihingen",
+    "Heinze|Maria|ST_5525|Bad Cannstatt",
+    "Herrman|Georg|FR_2536|Merzhausen",
+    ]
+
+schneider = [
+    "Schneider|Wolfgang|Heidelberg|HE_20011",
+    "Bartels|Martina|Sandhausen|HE_15436",
+    "Beck|Hans|Neckarsteinach|HE_5436",
+    ]
+def fof(listenname, sep):
+    for line in listenname:
+        splited_line = line.split("|")
+        splited_line.insert(0, splited_line[sep])
+        splited_line.pop(sep + 1)
+        string=" ".join(splited_line)
+        neue_liste.append(string)    
+    return(neue_liste)
+print(fof(wirth, 2))
+print(fof(schneider, 3))
