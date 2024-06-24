1754701eadf7a4edce766074bd225a3fb054428f
jethi05 <jethi.gei@gmail.com>
2024-06-24 08:40:55 +0200

    used pylint to make code a 10/10

--- 1.lehrjahr/ka4/personalnummer_zuerst.py
+++ 1.lehrjahr/ka4/personalnummer_zuerst.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+'''Programm um Die Personalnummer als erstes anzuzeigen'''
 neue_liste=[]
 wirth = [
     "Mueller|Josef|FR_1112|Freiburg",
@@ -6,19 +6,19 @@ wirth = [
     "Heinze|Maria|ST_5525|Bad Cannstatt",
     "Herrman|Georg|FR_2536|Merzhausen",
     ]
-
 schneider = [
     "Schneider|Wolfgang|Heidelberg|HE_20011",
     "Bartels|Martina|Sandhausen|HE_15436",
     "Beck|Hans|Neckarsteinach|HE_5436",
     ]
 def fof(listenname, sep):
+    '''Personalnummer zuerst hinschreiben'''
     for line in listenname:
         splited_line = line.split("|")
         splited_line.insert(0, splited_line[sep])
         splited_line.pop(sep + 1)
         string=" ".join(splited_line)
-        neue_liste.append(string)    
-    return(neue_liste)
+        neue_liste.append(string)
+    return neue_liste
 print(fof(wirth, 2))
 print(fof(schneider, 3))
