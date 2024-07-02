54b199cd691d9d6398c0ada52118f41a187da1a9
jethi05 <jethi.gei@gmail.com>
2024-07-02 07:42:06 +0200

    Datei wurde gefinished

--- 1.lehrjahr/ka4/datei_einlesen/main.py
+++ 1.lehrjahr/ka4/datei_einlesen/main.py
@@ -1,18 +1,15 @@
 #!/usr/bin/python3
-
-erste_zeile = 1
+erste_zeile = True
 with open("bundeslaender.csv", "r") as file:
-    lines = file.readlines()  # Read all lines into a list
-
+    lines = file.readlines()
 with open("bundeslaender_erweitert.csv", "w") as end_file:
     for line in lines:
-        line = line.strip().split(",")  # Split the line into a list
-        if erste_zeile == 1:
+        line = line.strip().split(",")
+        if erste_zeile:
             line.append("Einwohner je qKM")
-            erste_zeile = 0
+            erste_zeile = False
         else:
             einwohner_je_qkm = int(line[1]) / int(line[2])
             line.append(str(einwohner_je_qkm))
-        end_file.write(",".join(line) + "\n")  # Join the list back into a string and write to the file
+        end_file.write(",".join(line) + "\n")
 print("Done")
-
