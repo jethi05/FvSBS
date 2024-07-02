2a07a81d037b24dbe6d2093c324042ba9b28d2d1
jethi05 <jethi.gei@gmail.com>
2024-07-02 08:00:21 +0200

    README von Herr Thiersch geupdatet

--- 1.lehrjahr/ka4/datei_einlesen/README.md
+++ 1.lehrjahr/ka4/datei_einlesen/README.md
@@ -1,7 +1,9 @@
 # Nenne was zu tun ist
 wir müssen einfach nur die Datei einlesen und dann die Bewohner durch die Fläche Teilen und runter rechnen
 # Nenne Teilschritte
-- datei einlesen
-- fläche durch Bewohner teilen
-- datei mit w in neue Datei schreiben
-- datei schließen
+- Datei öffnen -with open() as datei
+- Datei zeilenweise einlesen - datei.readlines()
+- An Komma zerlegen - .split(",")
+- Neuen Wert durch Teilen berechnen - /
+- Neuen Wert in Liste anfügen - .append()
+- String aus Liste erstellen und in Enddatei anfügen
--- 1.lehrjahr/ka4/datei_einlesen/main.py
+++ 1.lehrjahr/ka4/datei_einlesen/main.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
-erste_zeile = True
-with open("bundeslaender.csv", "r") as file:
+'''Dieses Programm dient dazu, die Personen pro KM^2 je Bundesland Wohnen'''
+ERSTE_ZEILE = True
+with open("bundeslaender.csv", "r", encoding="utf8") as file:
     lines = file.readlines()
-with open("bundeslaender_erweitert.csv", "w") as end_file:
+with open("bundeslaender_erweitert.csv", "w", encoding="utf8") as end_file:
     for line in lines:
         line = line.strip().split(",")
-        if erste_zeile:
+        if ERSTE_ZEILE:
             line.append("Einwohner je qKM")
-            erste_zeile = False
+            ERSTE_ZEILE = False
         else:
             einwohner_je_qkm = int(line[1]) / int(line[2])
             line.append(str(einwohner_je_qkm))
