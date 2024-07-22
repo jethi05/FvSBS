d71f350252d962d781ac1442b8b3184049e26d40
Bewerk <140491321+Bewerk@users.noreply.github.com>
2024-07-22 11:05:04 +0200

    Füge Bevölkerungsdichte hinzu
    Gemäß neuer Aufgabenbeschreibung. Räume außerdem den Code auf

--- 1.lehrjahr/ka4/datei_einlesen/partIII/bundeslaender.py
+++ 1.lehrjahr/ka4/datei_einlesen/partIII/bundeslaender.py
@@ -1,5 +1,4 @@
-def calc_einwohner_pro_km(zeile):
-    print(zeile[2], zeile[1])
+def calc_einwohner_pro_km(zeile: list[str]) -> str:
     return str(round(int(zeile[2]) / int(zeile[1]),2))
 
 def calc_anteil_gesamtflaeche(zeile: list[str], gesamtflaeche:int) -> str:
@@ -8,11 +7,17 @@ def calc_anteil_gesamtflaeche(zeile: list[str], gesamtflaeche:int) -> str:
 def calc_anteil_bevoelkerung(zeile: list[str], gesamtbevoelkerung:int) -> str:
     return str(round(int(zeile[2]) / gesamtbevoelkerung,3))
 
+def calc_bevoelkerungsdichte(einwohner_pro_km: float, gesamtdichte:int) -> str:
+    if (einwohner_pro_km > gesamtdichte):
+        return 'hoch'
+    else:
+        return 'gering'
+
 def main():
     with open("./bundeslaender.csv", "r") as laenderdatei:
         csv = laenderdatei.read().split('\n')
     
-    csv[0] += ',Einwohner/Km,Anteil an Gesamtfläche,Anteil an Gesamtbevölkerung' # Bevölkerungsdichte == Einwohner/km
+    csv[0] += ',Einwohner/Km,Anteil an Gesamtfläche,Anteil an Gesamtbevölkerung,Bevölkerungsdichte'
     
     gesamtflaeche = 0
     gesamtbevoelkerung = 0
@@ -22,14 +27,21 @@ def main():
         gesamtflaeche += int(zeile[1])
         gesamtbevoelkerung += int(zeile[2])
         
+    gesamtdichte = gesamtbevoelkerung /  gesamtflaeche
+    
+        
     for i in range(1,len(csv)):
         zeile = csv[i].split(',')
+        
         einwohner_pro_km = calc_einwohner_pro_km(zeile)
         flaechenanteil = calc_anteil_gesamtflaeche(zeile, gesamtflaeche)
         bevoelkerungsanteil = calc_anteil_bevoelkerung(zeile, gesamtbevoelkerung)
+        bevoelkerungsdichte = calc_bevoelkerungsdichte(float(einwohner_pro_km),gesamtdichte)
+        
         zeile.append(einwohner_pro_km)
         zeile.append(flaechenanteil)
         zeile.append(bevoelkerungsanteil)
+        zeile.append(bevoelkerungsdichte)
         
         csv[i] = ','.join(zeile)
 
