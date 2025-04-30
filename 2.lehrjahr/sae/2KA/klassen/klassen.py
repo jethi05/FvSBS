b3e2a646775914aaca3da86fa4e2d69d4341254a
jethi05 <jethi.gei@gmail.com>
2025-04-30 15:21:53 +0200

    update Readme

---
+++ 2.lehrjahr/sae/2KA/klassen/klassen.py
@@ -0,0 +1,23 @@
+# Klassen immer CammelCase
+class Stift:
+
+    # __init__ ist ein Konstruktur, dadurch werden die Startwerte gesetzt
+    # self ist eine Selbstreferenz -> wie "Ich" in der Sprache
+    # Attribut == Variable
+    def __init__(self, farbe):
+        self.farbe = farbe
+
+    def schreibe(self, text):
+        print(f"{self.farbe}-farbender Stift schreibt {text}")
+
+#rotstift = Stift("rot")
+#rotstift.schreibe("Hallo Klasse")
+
+class Tier:
+    def __init__(self, laut):
+        self.laut = laut
+
+    def gib_laut(self):
+        print(f"Tier gibt laut {self.laut}")
+hund = Tier("Wuff")
+hund.gib_laut()
